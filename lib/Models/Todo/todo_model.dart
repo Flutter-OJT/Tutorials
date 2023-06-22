55b6b72b48cec6a98fd8e3612c520200449d86f3
scm-sai-zaw-myint <scm.saizawmyint@gmail.com>
2023-06-22 09:53:40 +0630

    Revert "added tutorial04"
    This reverts commit a6f174fe88193aef4e2d1b864408ee068697d70c.

--- lib/Models/Todo/todo_model.dart
+++ lib/Models/Todo/todo_model.dart
@@ -1,12 +1,1 @@
-class ListItem {
-  int id;
-  String title;
-  String description;
-
-  ListItem({required this.id, required this.title, required this.description});
-
-  @override
-  String toString() {
-    return '{id: $id, title: $title, description: $description}';
-  }
-}
+// todo model
